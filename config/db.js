47df17727112739e62c83e723c792d1a6cae9081
Himanshu Dahare <himanshudahare6400@gmail.com>
2023-03-23 19:04:41 +0530

    feature/node-Auth

---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports ={
+    'secret' : "10xauthsecret",
+    // 'db':"mongodb://localhost:27017/node-auth"
+    'db':"mongodb+srv://himanshudahare:<password>@cluster0.adtkqwf.mongodb.net/?retryWrites=true&w=majority"
+}
