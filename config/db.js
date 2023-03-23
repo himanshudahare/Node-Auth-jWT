61c9cf3d2014d2e1c750ff40dc791fd7b027141e
Himanshu Dahare <himanshudahare6400@gmail.com>
2023-03-23 19:24:36 +0530

    added my file

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports ={
     'secret' : "10xauthsecret",
     // 'db':"mongodb://localhost:27017/node-auth"
-    'db':"mongodb+srv://himanshudahare:<password>@cluster0.adtkqwf.mongodb.net/?retryWrites=true&w=majority"
+    'db':process.env.DB_URL
 }
